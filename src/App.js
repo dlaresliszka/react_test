808f3bcca9ac2b38e668f9824b29310fdaf9770d
davidlares <david.lares@geopagos.com>
2019-07-31 11:16:07 -0300

    added checkout form and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './navbar.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import  checkout from './pages/checkout/checkout';
+
+function index() {
+  return <h2>Inicio</h2>;
+}
+
+function products() {
+  return <h2>Productos</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <div className="topnav">
+            <Link className="active" to="/">Inicio</Link>
+            <Link to="/checkout/">Checkout</Link>
+            <Link to="/products/">Products</Link>
+          </div>
+
+          <Route path="/" exact component={index} />
+          <Route path="/checkout/" component={checkout} />
+          <Route path="/products/" component={products} />
+        </div>
+      </Router>
   );
 }
 
